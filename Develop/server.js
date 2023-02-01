77805ce1282ad9ca282f5d790691b0f6518c41fe
Kristen Schaefer <kristencschaefer@gmail.com>
2023-02-01 15:13:40 -0600

    added deletion function

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { json } = require('express');
 const db = "./db/db.json";
 const PORT = 3001;
 
 const app = express();
 
-function generateId(){
+function generateId() {
   return Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
     .substring(1);
@@ -21,9 +22,9 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
- app.get('/notes', (req, res) =>
-   res.sendFile(path.join(__dirname, '/public/notes.html'))
- );
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 // GET request for notes
 app.get('/api/notes', (req, res) => {
@@ -40,99 +41,17 @@ app.post('/api/notes', (req, res) => {
   const newItem = req.body;
   newItem.id = generateId();
   json.push(newItem);
-  fs.writeFileSync(db,JSON.stringify(json));
+  fs.writeFileSync(db, JSON.stringify(json));
   res.status(201).json(json);
 });
 
-// GET request for reviews
-app.get('/api/reviews', (req, res) => {
-  console.info(`GET /api/reviews`);
-  res.status(200).json(reviews);
-});
-
-// GET request for a single review
-app.get('/api/reviews/:review_id', (req, res) => {
-  if (req.params.review_id) {
-    console.info(`${req.method} request received to get a single a review`);
-    const reviewId = req.params.review_id;
-    for (let i = 0; i < reviews.length; i++) {
-      const currentReview = reviews[i];
-      if (currentReview.review_id === reviewId) {
-        res.json(currentReview);
-        return;
-      }
-    }
-    res.status(404).send('Review not found');
-  } else {
-    res.status(400).send('Review ID not provided');
-  }
-});
-
-// POST request to add a review
-app.post('/api/reviews', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to add a review`);
-
-  // TODO: Add a comment describing the functionality of following line of code:
-  const { product, review, username } = req.body;
-
-  // TODO: Add a comment describing why we would check to see if the following properties exist before entering the code block
-  if (product && review && username) {
-    // Variable for the object we will save
-    const newReview = {
-      product,
-      review,
-      username,
-      upvotes: Math.floor(Math.random() * 100),
-      review_id: uuid(),
-    };
-
-    const response = {
-      status: 'success',
-      body: newReview,
-    };
-
-    console.log(response);
-
-    // TODO: Add a comment explaining the functionality of res.json()
-    res.status(201).json(response);
-  } else {
-    // TODO: Add a comment describing the purpose of the else statement in this POST request.
-    res.status(500).json('Error in posting review');
-  }
-});
-
-// GET request for a specific review's upvotes
-app.get('/api/upvotes/:review_id', (req, res) => {
-  console.info(`${req.method} request received to get upvotes for a review`);
-  for (let i = 0; i < reviews.length; i++) {
-    const currentReview = reviews[i];
-    if (currentReview.review_id === req.params.review_id) {
-      res.json({
-        message: `The review with ID ${currentReview.review_id} has ${currentReview.upvotes}`,
-        upvotes: currentReview.upvotes,
-      });
-      return;
-    }
-  }
-  res.json('Review ID not found');
-});
-
-// POST request to upvote a review
-app.post('/api/upvotes/:review_id', (req, res) => {
-  if (req.body && req.params.review_id) {
-    console.info(`${req.method} request received to upvote a review`);
-    const reviewId = req.params.review_id;
-    for (let i = 0; i < reviews.length; i++) {
-      const currentReview = reviews[i];
-      if (currentReview.review_id === reviewId) {
-        currentReview.upvotes += 1;
-        res.json(`New upvote count is: ${currentReview.upvotes}!`);
-        return;
-      }
-    }
-    res.json('Review ID not found');
-  }
+app.delete('/api/notes/:id', (req, res) => {
+  const fileContents = fs.readFileSync(db);
+  const json = JSON.parse(fileContents);
+  const id = req.params.id;
+  const newjson = json.filter(note => note.id != id)
+  fs.writeFileSync(db, JSON.stringify(newjson));
+  res.status(200).json(newjson);
 });
 
 app.listen(PORT, () =>
